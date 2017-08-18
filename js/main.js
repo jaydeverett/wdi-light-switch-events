ba1b3a7437aff5c7a2580f06a1fc451332e721ab
Jay Deverett <jayjdeverett@gmail.com>
2017-08-18 15:49:41 -0400

    add working light switch on/off functionality

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+var lightswitch = document.querySelector('.switch');
+var light = document.querySelector('.light');
+var text = document.querySelector('h1')
 
-
+lightswitch.addEventListener('click', function() {
+  if (lightswitch.className === 'switch off') {
+    lightswitch.className = 'switch on';
+    light.className = 'light';
+    text.innerText = 'It\'s so bright in here!';
+  }
+  else if (lightswitch.className === 'switch on') {
+    lightswitch.className = 'switch off';
+    light.className = 'dark';
+    text.innerText = 'Hey, who turned the lights off?';
+  }
+})
